32fab7ea5576f998774757de3c5aa2ddbb6bc99a
Cristiano Benjamin <csbenjamin.math@gmail.com>
2014-02-09 23:34:06 -0200

    add example for use of request

--- controllers/hello.php
+++ controllers/hello.php
@@ -7,6 +7,10 @@ class hello extends controller {
         $this->response->foo->bar = 123;
         $this->response->params = func_get_args();
         $this->response->hello = $name;
+        $this->response->method = $this->request->getMethod();
+        if($this->request->getMethod() == "post"){
+            $this->response->post_data = $this->request->getBody();
+        }
     }
 
 }
